47ee02c57705c70dcd2deb694ed45b7f6d6721ec
Paul B <paul@bonaud.fr>
2017-04-30 13:18:47 +0200

    deploy: revoke old deploy_key and deploy only necessary files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -e
 
-# Commit middleman build directory
-git clean -dfx -e public -e deploy_key.pem
-mv public/* .
-git add .
-git commit -m "deploy: publishing github pages $(date)"
-
 ssh_repo="git@github.com:paulRbr/paulrbr.github.io.git"
 
+# Commit middleman build directory
+git clean -dfx -e public -e deploy_travis.pem
+cd public && ls -la
+git init
+git add .
+git config --global user.email "deploy@paul.bonaud.fr"
+git config --global user.name "Deploy bot"
+git commit -m "deploy: publishing github pages $(date)"
+
 # Push to gh-pages branch!
-git remote set-url origin $ssh_repo
+git remote add origin $ssh_repo
 git push -f origin HEAD:master
