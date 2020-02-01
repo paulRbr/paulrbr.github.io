94583f2c7b350c81de77b15c3ec1db7e6eef96d6
Paul B <paul@bonaud.fr>
2020-02-01 01:20:25 +0100

    upgrades: ruby 2.6, bundle upgrade, hack for rack 2.1.0...

--- Gemfile
+++ Gemfile
@@ -1,24 +1,26 @@
 # If you do not have OpenSSL installed, update
 # the following line to use "http://" instead
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
 gem "middleman"
 # Bring back sprockets to Middleman
-gem 'middleman-sprockets', '~> 4.0.0.rc.1'
+gem "middleman-sprockets", "~> 4.0.0.rc.1"
 # Blogging extension
 gem "middleman-blog"
-gem "builder"  # For Atom xml feed
+gem "builder" # For Atom xml feed
 gem "nokogiri" # For xml/html processing
 # Client side search
-gem 'middleman-search'
+gem "middleman-search"
 # Live-reloading plugin
 gem "middleman-livereload"
 # Font-awesome style
-gem 'font-awesome-middleman'
+gem "font-awesome-middleman"
 # Disqus comments
 gem "middleman-disqus"
 # Color syntax
 gem "middleman-syntax"
+# Minify HTML
+gem "middleman-minify-html"
 # Org mode parser
 gem "org-ruby"
 
--- config.rb
+++ config.rb
@@ -14,9 +14,9 @@
 # Per-page layout changes:
 #
 # With no layout
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
+page "/*.xml", layout: false
+page "/*.json", layout: false
+page "/*.txt", layout: false
 
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
@@ -63,22 +63,22 @@ end
 ###
 
 activate :search do |search|
-  search.resources = ['blog/']
+  search.resources = ["blog/"]
   search.fields = {
-    title:   {boost: 100, store: true, required: true},
+    title: {boost: 100, store: true, required: true},
     content: {boost: 50},
-    url:     {index: false, store: true},
-    author:  {boost: 30}
+    url: {index: false, store: true},
+    author: {boost: 30},
   }
 end
 
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-set :fonts_dir,  'fonts'
+set :css_dir, "stylesheets"
+set :js_dir, "javascripts"
+set :images_dir, "images"
+set :fonts_dir, "fonts"
 
 set :markdown_engine, :kramdown
-activate :syntax #, line_numbers: true
+activate :syntax # , line_numbers: true
 
 activate :sprockets
 
@@ -94,16 +94,19 @@ configure :development do
 end
 # Build-specific configuration
 configure :build do
-  set :build_dir, 'public'
+  set :build_dir, "public"
   set :base_url, "" # baseurl for GitLab Pages (project name) - leave empty if you're building a user/group website
-  set :site_url, 'https://paulrbr.io'
-  set :websocket_url, 'wss://paulrbr.io/status'
+  set :site_url, "https://paulrbr.io"
+  set :websocket_url, "wss://paulrbr.io/status"
   activate :relative_assets # Use relative URLs
   # For example, change the Compass output style for deployment
   activate :minify_css
 
   # Minify Javascript on build
   activate :minify_javascript
+
+  # Minify HTML
+  activate :minify_html
 
   activate :disqus do |d|
     # using a different shortname for production builds
@@ -118,3 +121,26 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+# Ugly bug between Middleman and Rack 2.1.0
+class ZapContentLength < Struct.new(:app)
+  def call(env)
+    s, h, b = app.call(env)
+    # The URL rewriters in Middleman do not update Content-Length correctly,
+    # which makes Rack-Lint flag the responses as having a wrong Content-Length.
+    # For building assets this has zero importance because the Content-Length
+    # header will be discarded - it is the server that recomputes it. But
+    # it does prevent the site from building correctly.
+    #
+    # The fastest way out of this is to let Rack recompute the Content-Length
+    # forcibly, for every response, at retrieval time.
+    #
+    # See https://github.com/middleman/middleman/issues/2309
+    # and https://github.com/rack/rack/issues/1472
+    h.delete("Content-Length")
+    [s, h, b]
+  end
+end
+
+app.use ::Rack::ContentLength
+app.use ZapContentLength
